9520c9c0b6a2b1dab1ddf38d447b4e8024a62cbc
Hugo <hugo.cridlig@esme.fr>
2022-02-21 13:37:04 +0100

    add comment for later change

--- Python/read_arduino.py
+++ Python/read_arduino.py
@@ -9,29 +9,34 @@ def connection():
     connect = mysql.connector.connect(host="10.4.253.110", user="connect", password="", database="projet_btssnir")
     cursor= connect.cursor()
 
-    return connect, cursor
+    return connect, cursor ##Enlever connect
 
 
 
 def request(req):
-    connection()
-    global myresult
+    connection()  ##Connection appeler une fois en début de programme puis utiliser le return dans une autre variable
+    global myresult  ##Pas besoin de myresult
 
     cursor.execute(req)
-    myresult=cursor.fetchall()
+    myresult=cursor.fetchall() ##Pas besoin de myresult
 
-    return myresult
+    return myresult  ##Pas besoin de return
 
 
 ser = serial.Serial('COM5', 9800, timeout=1)
 
-while 1:
-    line = ser.readline()   # read a byte
-    if line:
-        string = line.decode()  # convert the byte string to a unicode string
-        #num = int(string) # convert the unicode string to an int
-        print(string)
-        request("INSERT INTO `test` (`id`, `uid`) VALUES (NULL, '{}')".format(string))
+def main():
+    while 1:
+        line = ser.readline()   # read a byte
+        if line:
+            string = line.decode()  # convert the byte string to a unicode string
+            #num = int(string) # convert the unicode string to an int
+            print(string)
+            request("INSERT INTO `test` (`id`, `uid`) VALUES (NULL, '{}')".format(string))
     
 
 ser.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Read data from the arduino and send it to the remote database")
+    main()
