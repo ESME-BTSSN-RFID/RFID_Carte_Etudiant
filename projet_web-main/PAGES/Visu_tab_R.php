5e69d7fc4bae4f1878533c800c01089aaffbee01
Hugo <hugo.cridlig@esme.fr>
2022-03-11 16:20:24 +0100

    input week

--- projet_web-main/PAGES/Visu_tab_R.php
+++ projet_web-main/PAGES/Visu_tab_R.php
@@ -45,19 +45,37 @@ if (isset($_SESSION['idCand'])){
             $cnx=Connexion("localhost", "projet_btssnir", "root", "");
             $req = "SELECT * FROM classe";
             $result=requeteSelect($cnx, $req);
-        ?>
-        <select name="classe" >
-            <option value="">--Choisir une classe--</option>
-            <?php
-                foreach($result as $row){
-                    ?>
-                    <option value="<?php echo $row['idClass'];?>"><?php echo $row['label'];?></option>
-                    <?php
-                }
-            ?>
+
+            $first_of_week = date("Y-m-d", strtotime('monday this week'));
+            $last_of_week = date("Y-m-d", strtotime('sunday this week'));
+            $first_month = date("m", strtotime('monday this week'));
+            $last_month = date("m", strtotime('sunday this week'));
             
+            $month = array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+            $first_month = $month[intval($first_month)-1];
+            $last_month = $month[intval($last_month)-1];
+
+            $first_date = date("j ", strtotime($first_of_week));
+            $last_date = date("j ", strtotime($last_of_week));
+
+        
+        ?>
+
+        <form action="">
+            <select name="classe" >
+                <option value="">--Choisir une classe--</option>
+                <?php
+                    foreach($result as $row){
+                        ?>
+                        <option value="<?php echo $row['idClass'];?>"><?php echo $row['label'];?></option>
+                        <?php
+                    }
+                ?>
         
         </select>
+        
+        <p><button class="button"> <i class="arrow left"></i></button> Semaine du <?php echo $first_date.$first_month?> au <?php echo $last_date.$last_month?> <button class="button"> <i class="arrow right"></i></button></p>
+        </form>
 
         <table class="tableau">
             <tr>
@@ -72,9 +90,7 @@ if (isset($_SESSION['idCand'])){
             </tr>
 
             <?php   
-            $first_of_week = date("Y-m-d", strtotime('monday this week'));
-            $last_of_week = date("Y-m-d", strtotime('sunday this week'));
-            
+                        
             $cnx=Connexion("localhost", "projet_btssnir", "root", "");
             $req = "SELECT s.idSeance, p.nomProf, m.nom, c.label, s.dateDebut, s.dateFin FROM seance s INNER JOIN professeur p ON s.idProf=p.idProf INNER JOIN matiere m ON s.idMatiere=m.idMatiere INNER JOIN classe c ON s.idClasse=c.idClass WHERE dateDebut>='$first_of_week''T00:00' AND dateFin<='$last_of_week''T23:59' ORDER BY dateDebut";
             $result=requeteSelect($cnx, $req);
